b5795c070e4e9cf3d904efc55d230295cdf042df
Isi Okojie <Isi@Isis-MacBook-Pro-2.local>
2019-03-16 00:46:38 -0700

    Pull user's playlists

---
+++ Pods/SpotifyKit/SpotifyInfo.swift
@@ -0,0 +1,8 @@
+//
+//  SpotifyInfo.swift
+//  Pods-music.clean
+//
+//  Created by Isi Okojie on 3/15/19.
+//
+
+import Foundation
---
+++ music.clean/PlaylistsViewContoller.swift
@@ -0,0 +1,9 @@
+//
+//  PlaylistsViewContoller.swift
+//  music.clean
+//
+//  Created by Isi Okojie on 3/15/19.
+//  Copyright © 2019 Isi Okojie. All rights reserved.
+//
+
+import Foundation
