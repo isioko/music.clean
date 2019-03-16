5f4d9ad297d360e7646b7778c3f908bdbbcbfe91
Isi Okojie <Isi@isis-macbook-pro-2.local>
2019-03-15 22:58:07 -0700

    Set up SpotifyManager

--- music.clean/AppDelegate.swift
+++ music.clean/AppDelegate.swift
@@ -8,14 +8,42 @@
 
 import UIKit
 
+// Import SpotifyKit iOS library
+import SpotifyKit
+
+// MARK: SpotifyKit initialization
+
+// The Spotify developer application object
+// Fill this with the data from the app you've set up on Spotify developer page
+fileprivate let application = SpotifyManager.SpotifyDeveloperApplication(
+    clientId:     SpotifyAppInfo.clientID,
+    clientSecret: SpotifyAppInfo.clientSecret,
+    redirectUri:  SpotifyAppInfo.redirectURI
+)
+
+// The SpotifyKit helper object that will allow you to perform the queries
+let spotifyManager = SpotifyManager(with: application)
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    // MARK: URL event handling
+    
+    /**
+     After sending 'swiftify.authorize()' command,
+     our application receives an URL starting with the "redirect uri" we've set up
+     in Spotify Developer page and added to our app's Info.plist under "URL types" -> "URL schemes".
+     This URI contains the token access code which grants the privileges needed for performing Spotify queries.
+     Here we catch the URI as it is passed to our app, retrieve the token code and send it
+     to Swifify, which will generate the code and save it in Keychain for persistency
+     */
+    func application(_ application: UIApplication, handleOpen url: URL) -> Bool {
+        print(url)
+        spotifyManager.saveToken(from: url)
         return true
     }
 
