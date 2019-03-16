6fa4154d9c1ddf7dad1de597cd4ac6a3eb46581a
Isi Okojie <Isi@isis-macbook-pro-2.local>
2019-03-15 22:03:57 -0700

    Initial Commit

---
+++ music.clean/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  music.clean
+//
+//  Created by Isi Okojie on 3/15/19.
+//  Copyright © 2019 Isi Okojie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
