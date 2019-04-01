91f195e417b9a74b1283b86431a745b8b1a9d6fd
Isi Okojie <Isi@Isis-MacBook-Pro-2.local>
2019-04-01 16:15:47 -0700

    add trackURI info and filter tracks

---
+++ music.clean/MakeCleanPlaylistViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MakeCleanPlaylistViewController.swift
+//  music.clean
+//
+//  Created by Isi Okojie on 3/31/19.
+//  Copyright © 2019 Isi Okojie. All rights reserved.
+//
+
+import Foundation
